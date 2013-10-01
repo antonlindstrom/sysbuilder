c52290ef73c6fc2c49861eb096bdc845fb99ea4a
Anton Lindström <lindztr@gmail.com>
2013-10-01 16:40:49 +0000

    add failure scenarios

--- create_vm.sh
+++ create_vm.sh
@@ -11,7 +11,18 @@ fi
 echo "Starting build of: $VM_HOSTNAME"
 
 bash ./sysprep.sh $VM_HOSTNAME
+
+if [ $? != 0 ]; then
+  echo "Could not create image!"
+  exit 2
+fi
+
 bash ./build_template.sh $VM_HOSTNAME
+
+if [ $? != 0 ]; then
+  echo "Could not create template!"
+  exit 2
+fi
 
 mv $VM_HOSTNAME.qcow /mnt/storage/vms/$VM_HOSTNAME.qcow
 
